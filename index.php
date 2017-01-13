e9468cbed7f14e4f6c887528962e591280bdf104
Anton <a.kritikos1997@gmail.com>
2017-01-13 11:56:15 +0100

    Update

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
   include "views/head.html";
+  include "views/header.html";
   switch ($_GET) {
     case 'value':
       # code...
       break;
 
     default:
+      include "views/fish.html";
       include "views/home.html";
       break;
   }
 
 
-
+  include "views/footer.html";
 ?>
--- js/article.js
+++ js/article.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
+  var article,number,container;
+  h1Check();
   $(document).mousedown(function(event){
-    if (event.target != article && $(event.target).prop("tagName") != "A"){
-      $(".article").not($(event.target).parent()).removeClass("open");
-      $(".article").not($(event.target).parent()).children().addClass("hidden");
-    }
-    var article = $(event.target).parent();
-    var dataId = $(article).data("id");
-    if($(article).hasClass("article")){
-      $(article).toggleClass("open");
-      $(article).children().toggleClass("hidden");
+    article = $(event.target);
+
+    if ($(article).data("content") || $(article).parent().data("content") && !$(event.target).closest('a').length) {
+      number = $(article).data("content") || $(article).parent().data("content");
+      container = $("div").find("[data-container='" + number + "']");
+      $(container).toggleClass("open");
+      $(container).children().toggleClass("hidden");
+      $(".article").not(container).removeClass("open");
+      $(".article").not(container).children().addClass("hidden");
     }
   });
+  $(document).keydown(function(event){
+    if (event.keyCode == 9) {
+        event.preventDefault();
+    }
+  });
+  $(document).resize(function(){
+    // h1Check();
+  });
+  function h1Check(){
+    var h1Height = $(".text").children("h1");
+    for (var i = 0; i <   $(".text").children("h1").length; i++) {
+      if (parseInt($(h1Height).slice(i).outerHeight()) > 60) {
+        $(".text").children("h1").slice(i,(i+1)).css("line-height","30px");
+      }
+    }
+  }
 });
