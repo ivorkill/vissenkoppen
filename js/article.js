3c7ee3e65835e90ef10463ed949b7f674622b720
Anton <a.kritikos1997@gmail.com>
2017-02-06 10:23:06 +0100

    final product

--- js/article.js
+++ js/article.js
@@ -9,19 +9,11 @@ $(document).ready(function(){
       container = $("div").find("[data-container='" + number + "']");
       $(container).toggleClass("open");
       $(".article").not(container).removeClass("open");
-      // $(".article").not(container).children().addClass("hidden");
-      // console.log(article);
-      // $(container).children(".text").children("h1").css("line-height","60px");
-      // $(".article").not(container).children(".text").children("h1").css("line-height","30px");
     }
-    // console.log($(article).parent().data("content"));
   });
   $(document).keydown(function(event){
     if (event.keyCode == 9) {
         event.preventDefault();
     }
-  });
-  $(document).resize(function(){
-    // h1Check();
   });
 });
